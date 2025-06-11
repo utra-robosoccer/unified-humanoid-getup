a835cf6b4fd2ba5b83f0195aac1c0fef2441e970
JonathanSpraggett <jonathanspraggett@gmail.com>
2025-06-11 17:34:38 -0400

    updated plots

--- collect_data.py
+++ collect_data.py
@@ -7,8 +7,9 @@ import numpy as np
 
 def main():
     # List all the robot names you want to sweep over:
-    multiplier1 = 7  # number of robots
-    multiplier2 = 10  # number of seeds per row
+    # TODO make this better and easier to understnand
+    multiplier1 = 7 # number of robots
+    multiplier2 = 10 # number of seeds per row
     multiplier3 = 1  # number of rows
     multiplier = multiplier2 * multiplier3
     robot_names = [
@@ -22,16 +23,17 @@ def main():
         [['nugus']] * multiplier,
     ]
     robot_names = reduce(lambda x, y: x + y, robot_names)
-    ids = [9, 16, 23, 30] * 7
+    ids = [9,16,23,30]*7
     ids = []
     for i in range(multiplier3):
-        ids.append(np.linspace(56 + i, 65 + i, multiplier2, dtype=int))
-    ids = np.array(ids * multiplier1).flatten()
+        ids.append(np.linspace(63+i,72+i,multiplier2,dtype=int))
+    ids = np.array(ids*multiplier1).flatten()
     print(ids)
     print(robot_names)
     print(len(ids))
     print(len(robot_names))
-
+    # ids = [49,56,50,57,51,58,52,59,53,60,54,61,55,62]
+    #
     for idx, name in enumerate(robot_names):
         cmd = [
             "python3.10", "enjoy_sbx.py",
